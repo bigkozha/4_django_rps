d5efffed8f6992c1c89575368de4444ee8275fcc
nuraly zhanbyrbayev <bigkozha@gmail.com>
2020-07-17 20:50:49 +0600

    Add models.

--- game/models.py
+++ game/models.py
@@ -1,5 +1,13 @@
+from django.contrib import User
 from django.db import models
 
 
 class Game(models.Model):
     is_active = models.BooleanField(default=True)
+    winner = models.ForeignKey(User)
+
+
+class Move():
+    time = models.DateField(auto_now=False, auto_now_add=True)
+    game = models.ForeignKey(Game, on_delete=model.CASCADE, related_name='moves')
+    gamer = models.ForeignKey(User)
